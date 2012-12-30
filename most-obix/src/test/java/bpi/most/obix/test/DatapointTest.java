6c1776a2d8f1aaeffea606ae14d41fe630c5ecce
Alexej Strelzow <alexej.strelzow@gmail.com>
2012-12-30 17:20:43 +0100

    added Injection support

---
+++ most-obix/src/test/java/bpi/most/obix/test/DatapointTest.java
@@ -0,0 +1,11 @@
+package bpi.most.obix.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Besitzer
+ * Date: 30.12.12
+ * Time: 17:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class DatapointTest {
+}
