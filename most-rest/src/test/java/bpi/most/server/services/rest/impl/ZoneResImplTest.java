92922b275c9556be336b16a16efbfebf725a29c0
David <Administrator@128.131.197.244>
2013-01-04 20:04:28 +0100

    erster Test im REST-Modul (noch Fehlerhaft)

---
+++ most-rest/src/test/java/bpi/most/server/services/rest/impl/ZoneResImplTest.java
@@ -0,0 +1,36 @@
+package bpi.most.server.services.rest.impl;
+
+import javax.inject.Inject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for {@link bpi.most.server.services.rest.impl.ZoneResImpl}.
+ *
+ * @author David Bittermann
+ */
+//TODO ASE Jakob 
+//@ContextConfiguration(locations = "/WEB-INF/service.spring.xml")
+public class ZoneResImplTest { //extends AbstractTransactionalJUnit4SpringContextTests {
+    
+	@Inject
+	ZoneResImpl zoneRes;
+	
+	@Before
+    public void setUp() throws Exception {
+		zoneRes = new ZoneResImpl();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetZone() throws Exception {
+//        ZoneDTO zone = zoneRes.getZone(1);
+//        Assert.assertNotNull(zone);
+    }
+}
