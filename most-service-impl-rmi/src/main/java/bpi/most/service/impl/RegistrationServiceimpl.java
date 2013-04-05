48097ea1654fd9e21382ec20572d2e6dcff2a077
hare <hare@gmx.at>
2013-04-05 12:36:18 +0200

    starting implementation of virtual datapoint registration

--- most-service-impl-rmi/src/main/java/bpi/most/service/impl/RegistrationServiceimpl.java
+++ most-service-impl-rmi/src/main/java/bpi/most/service/impl/RegistrationServiceimpl.java
@@ -3,6 +3,7 @@ package bpi.most.service.impl;
 import bpi.most.service.api.RegistrationService;
 
 import java.net.URI;
+import java.util.List;
 
 /**
  *
@@ -15,6 +16,7 @@ public class RegistrationServiceimpl implements RegistrationService{
 
 
 
+
     @Override
     public void register(String vdpType, URI serviceEndpoint) {
         //To change body of implemented methods use File | Settings | File Templates.
@@ -23,5 +25,10 @@ public class RegistrationServiceimpl implements RegistrationService{
     @Override
     public void unregister(String vdpType, URI serviceEndpoint) {
         //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<URI> getServiceProvider(String vdpType) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
