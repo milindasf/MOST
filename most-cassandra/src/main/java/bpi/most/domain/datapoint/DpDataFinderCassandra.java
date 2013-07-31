b322727f7b1e271cc37e22e9039d923896644454
hare <hare@gmx.at>
2013-07-31 17:39:03 +0200

    added new module for nosql solution. added javalib for accessing cassandra

---
+++ most-cassandra/src/main/java/bpi/most/domain/datapoint/DpDataFinderCassandra.java
@@ -0,0 +1,74 @@
+package bpi.most.domain.datapoint;
+
+import bpi.most.dto.DpDataDTO;
+import me.prettyprint.hector.api.Cluster;
+import me.prettyprint.hector.api.factory.HFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: harald
+ * Date: 31.07.13
+ * Time: 16:35
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class DpDataFinderCassandra implements IDatapointDataFinder {
+
+    /**
+     * connects to cassandra
+     * @throws Exception
+     */
+    @PostConstruct
+    public void initIt() throws Exception {
+        Cluster myCluster = HFactory.getOrCreateCluster("test-cluster", "localhost:9160");
+    }
+
+    /**
+     * gracefully releases connections to Cassandra
+     * @throws Exception
+     */
+    @PreDestroy
+    public void cleanUp() throws Exception {
+
+    }
+
+    @Override
+    public DatapointDataVO getData(String dpName) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public DatapointDatasetVO getData(String dpName, Date starttime, Date endtime) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public DatapointDatasetVO getDataPeriodic(String dpName, Date starttime, Date endtime, Float period, int mode) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Integer getNumberOfValues(String dpName, Date starttime, Date endtime) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int addData(String dpName, DpDataDTO measurement) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int delData(String dpName) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int delData(String dpName, Date starttime, Date endtime) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ most-cassandra/src/test/java/bpi/most/domain/datapoint/DpDataFinderCassandraTest.java
@@ -0,0 +1,30 @@
+package bpi.most.domain.datapoint;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: harald
+ * Date: 31.07.13
+ * Time: 16:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:META-INF/most-cassandra.spring.xml"})
+public class DpDataFinderCassandraTest {
+
+    @Inject
+    private IDatapointDataFinder dpDataFinder;
+
+    @Test
+    public void testExistence(){
+        Assert.assertNotNull(dpDataFinder);
+    }
+}
